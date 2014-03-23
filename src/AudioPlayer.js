17dbd7898dbe8ed78f42e319df285e9955dfc87e
Jim Slattery <slattery.jim@gmail.com>
2014-03-22 21:50:36 -0400

    reduced repetition in AudioPlayer.prototype definition

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -3,30 +3,29 @@ function AudioPlayer(src) {
   this.audio.src = src;
 }
 
-AudioPlayer.prototype.stop = function() {
-  this.audio.load(null);
-}
+AudioPlayer.prototype = {
+  shutdown: function() { this.audio.load(null); },
 
-AudioPlayer.prototype.isPaused        = function() { return this.audio.paused;        }
-AudioPlayer.prototype.getCurrentTime  = function() { return this.audio.currentTime;   }
-AudioPlayer.prototype.getPlaybackRate = function() { return this.audio.playbackRate;  }
+  isPaused:        function() { return this.audio.paused;        },
+  getCurrentTime:  function() { return this.audio.currentTime;   },
+  getPlaybackRate: function() { return this.audio.playbackRate;  },
 
-AudioPlayer.prototype.play  = function() { this.audio.play();  }
-AudioPlayer.prototype.pause = function() { this.audio.pause(); }
-AudioPlayer.prototype.changeRate = function(rate) {
-  var thiz = this; 
-  this._pausingAndResuming(function() { 
-    thiz.audio.playbackRate = rate; 
-  })
-}
-AudioPlayer.prototype._pausingAndResuming = function(fn) {
-  if (this.audio.paused) {
-    fn();
-  } else {
-    this.pause();
-    fn();
-    this.play();
+  play:  function() { this.audio.play();  },
+  pause: function() { this.audio.pause(); },
+  changeRate: function(rate) {
+    var thiz = this; 
+    this._pausingAndResuming(function() { 
+      thiz.audio.playbackRate = rate; 
+    })
+  },
+  _pausingAndResuming: function(fn) {
+    if (this.audio.paused) {
+      fn();
+    } else {
+      this.pause();
+      fn();
+      this.play();
+    }
   }
-}
-
+};
 module.exports = AudioPlayer;
